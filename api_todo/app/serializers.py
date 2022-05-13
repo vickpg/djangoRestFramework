4643f4e47429873a6ca95137931d5ae54384918c
vickpg <pgomesvictoria@gmail.com>
2022-05-13 15:27:29 -0300

    edit file

--- api_todo/app/serializers.py
+++ api_todo/app/serializers.py
@@ -1,5 +1,7 @@
 from app.models import Todo
 from rest_framework import serializers
 
-class TodoSerializer(serializers.modelSerializer):
-    
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Todo
+        fields = ['id', 'name', 'done', 'created_date']
