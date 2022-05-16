b9ca7e5b8a44c960da7b8a9a52c4a7534e396685
vickpg <pgomesvictoria@gmail.com>
2022-05-16 10:24:45 -0300

    new response e request view

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from app.models import Todo
+from app.serializers import TodoSerializer
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+#views quando acessamos url's, ela são "chamadas". Evia dados e recebe dados. Primeiro método para acessar view será GET. Depois disso, precisamos registrar uma url que chame essa função quando for acessada.
+@api_view(['GET'])
+def todo_list(request):
+    todo = Todo.objects.all()
+    serializer = TodoSerializer(todo, many=True)
+    return Response(serializer.data)
+
+
+
