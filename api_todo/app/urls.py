b58ab02303c95ab2d5f8535d9e288c1c2c58f979
vickpg <pgomesvictoria@gmail.com>
2022-05-16 15:06:52 -0300

    new views set class

--- api_todo/app/urls.py
+++ api_todo/app/urls.py
@@ -1,8 +1,12 @@
-from app.views import TodoListAndCreate, TodoDetailChangeAndDelete
+from app.views import TodoViewSet
 
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('', TodoListAndCreate.as_view()),
-    path('<int:pk>/', TodoDetailChangeAndDelete.as_view()),
-]
+router = DefaultRouter()
+router.register(r'', TodoViewSet)
+urlpatterns = router.urls
+
+#urlpatterns = [
+    #path('', TodoListAndCreate.as_view()),
+    #path('<int:pk>/', TodoDetailChangeAndDelete.as_view()),
+#]
--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -3,18 +3,23 @@ from app.serializers import TodoSerializer
 
 from rest_framework import generics
 from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
-from rest_framework.views import APIView 
+from rest_framework.views import APIView
 
-class TodoListAndCreate(generics.ListCreateAPIView):
-   queryset = Todo.objects.all()
-   serializer_class = TodoSerializer
-
-class TodoDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
+class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+#class TodoListAndCreate(generics.ListCreateAPIView):
+    #queryset = Todo.objects.all()
+    #serializer_class = TodoSerializer
+
+#class TodoDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
+    #queryset = Todo.objects.all()
+    #serializer_class = TodoSerializer
 
 #class TodoListAndCreate(APIView):
     #def get(self, request):
