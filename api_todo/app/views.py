b75a5170e7ca051465bc2173e1a47bf99fc194b9
vickpg <pgomesvictoria@gmail.com>
2022-05-16 15:24:08 -0300

    delete rest_framework import

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -7,7 +7,6 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
