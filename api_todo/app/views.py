d4e29e0adc2d4d853d943f2098d9d4a3cbb7aebf
vickpg <pgomesvictoria@gmail.com>
2022-05-16 11:02:57 -0300

    POST method

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -1,15 +1,22 @@
 from app.models import Todo
 from app.serializers import TodoSerializer
 
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-#views quando acessamos url's, ela são "chamadas". Evia dados e recebe dados. Primeiro método para acessar view será GET. Depois disso, precisamos registrar uma url que chame essa função quando for acessada.
-@api_view(['GET'])
+#views quando acessamos url's, ela são "chamadas". Evia dados e recebe dados. Primeiro método para acessar view será GET. Depois disso, precisamos registrar uma url que chame essa função quando for acessada. Para usar os métodos criamos validações para saber quando chamar cada uma, em situações diferentes.
+@api_view(['GET', 'POST'])
 def todo_list(request):
-    todo = Todo.objects.all()
-    serializer = TodoSerializer(todo, many=True)
-    return Response(serializer.data)
-
+    if request.method == 'GET': 
+        todo = Todo.objects.all()
+        serializer = TodoSerializer(todo, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
